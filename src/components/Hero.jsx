b188d29b308c56bb426ca83002333bc2abc382c5
I.Chandana <indu.99.ic@gmail.com>
2023-03-14 07:58:53 +0530

    March14-001

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,6 +29,29 @@ const Hero = () => {
 
       {/* create first 3js react fiber canvas */}
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div
+            className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary 
+          flex justify-center items-start p-2"
+          >
+            {/* now we are using framer motion to create it. */}
+            <motion.div
+              animate={{
+                y: [0, 24, 0], // we animate just Y property
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 bg-secondary rounded-full mb-1"
+            />
+          </div>
+        </a>
+        {/* <p>Hello</p> */}
+      </div>
     </section>
   );
 };
